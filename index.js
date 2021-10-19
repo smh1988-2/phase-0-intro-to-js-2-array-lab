da239339f4ae41ed32203c76653d36187fd72629
smh1988-2 <seamhurley1988@gmail.com>
2021-10-18 23:37:03 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution here!
+const cats=["Milo", "Otis", "Garfield"];
+console.log(cats);
+
+function destructivelyAppendCat() {
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat() {
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat() {
+    const newCats = [
+        ...cats.slice(0),
+        "Broom",
+    ];
+        return newCats;
+}
+
+function prependCat() {
+    const topCats = ["Arnold", ...cats];
+        return topCats; 
+}
+
+function removeLastCat() {
+    const bottomCats = [...cats.slice(0,2)];
+    return bottomCats;
+}
+
+function removeFirstCat() {
+    const sideCats = [...cats.slice(1,3)];
+    return sideCats;
+}
